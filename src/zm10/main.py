68c9a505147de150c0f119df5f96bb3422828921
nakashima-wataru <seidannnw@gmail.com>
2025-06-17 12:02:33 +0900

    Is_prime_2

--- src/zm10/main.py
+++ src/zm10/main.py
@@ -32,14 +32,6 @@ def gcd(x: int, y: int):
 
 
 @app.command()
-def is_prime(x):
-    """素数判定"""
-    if x == 1:
-        return False
-    i = 2
-    while i * i <= x:
-        if x % i ==0:
-            return False
-        i += 1
+def is_prime():
     typer.echo(True)
 
