1d8c674b45d41dad21c5a3e8b54facc048fbd4b5
nakashima-wataru <seidannnw@gmail.com>
2025-07-29 11:18:41 +0900

    和暦に対応

--- src/zm10/semizemi.py
+++ src/zm10/semizemi.py
@@ -1,14 +1,42 @@
 import datetime
 
+WAREKI_MAP = {
+    'R': 2018, '令和': 2018,
+    'H': 1988, '平成': 1988,
+    'S': 1925, '昭和': 1925,
+    'T': 1911, '大正': 1911,
+    'M': 1867, '明治': 1867,
+}
+
+
 def main():
     print("生年月日を入力してください")
     birthdate = input("入力(yyyy/mm/dd)：")
 
     try:
         parts = birthdate.split("/")
-        birth_year = int(parts[0])
+
+        year_part = parts[0]
         birth_month = int(parts[1])
         birth_day = int(parts[2])
+
+        if year_part.isdigit():
+            birth_year = int(year_part)
+
+        else:
+            era = ""
+            era_year_str = ""
+            for i, char in enumerate(year_part):
+                if char.isdigit():
+                    era = year_part[:i].upper() 
+                    era_year_str = year_part[i:] 
+                    break
+
+            if era not in WAREKI_MAP:
+                return f"エラー: 不明な元号です。「{era}」"
+            
+            
+            birth_year = WAREKI_MAP[era] + int(era_year_str)
 
         dt = datetime.date.today()
         age = dt.year - birth_year
